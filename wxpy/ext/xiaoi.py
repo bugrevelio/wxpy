9b8e1b587f4c96cf7d9da81662e7b132ba1877d4
bugrevelio <bugrevelio@byom.de>
2018-03-12 14:34:02 -0700

    Replaced vulnerable functions and outdated dependencies

--- wxpy/ext/xiaoi.py
+++ wxpy/ext/xiaoi.py
@@ -63,12 +63,12 @@ class XiaoI(object):
         nonce = "4103657107305326101203516108016101205331"
 
         sha1 = "{0}:{1}:{2}".format(self.key, self.realm, self.secret).encode("utf-8")
-        sha1 = hashlib.sha1(sha1).hexdigest()
+        sha1 = hashlib.sha512(sha1).hexdigest()
         sha2 = "{0}:{1}".format(self.http_method, self.uri).encode("utf-8")
-        sha2 = hashlib.sha1(sha2).hexdigest()
+        sha2 = hashlib.sha512(sha2).hexdigest()
 
         signature = "{0}:{1}:{2}".format(sha1, nonce, sha2).encode("utf-8")
-        signature = hashlib.sha1(signature).hexdigest()
+        signature = hashlib.sha512(signature).hexdigest()
 
         ret = collections.namedtuple("signature_return", "signature nonce")
         ret.signature = signature
